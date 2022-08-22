3aec836e37a75e2200b984d3202bcd16b60243c6
Cxqaq <ryancx0909@gmail.com>
2022-08-22 23:07:45 +1000

    100

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,15 +6,21 @@ public class PrintAndHide : MonoBehaviour
 {
     public int i = 3;
     public Renderer rend;
+    public int random;
     // Start is called before the first frame update
     void Start()
     {
-        
+        random = Random.Range(200, 251);
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log(gameObject.name + ":" + ++i);
+        if ((gameObject.tag == "Red") && i == 100)
+            gameObject.SetActive(false);
+        if ((gameObject.tag == "Blue" && i == random))
+            rend.enabled = !rend.enabled;
+
     }
 }
